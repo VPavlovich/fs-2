bb06e0a984ef836e868587340ced5a3873de36a6
VP <vpsev@email.ua>
2023-10-20 09:04:08 +0300

    3 coomit

---
+++ front/src/component/button/index.js
@@ -0,0 +1,12 @@
+import "./index.css";
+import { Link } from 'react-router-dom';
+
+export default function Component({ classname,type,  id, text, color }) {
+  return (
+        <div class="buttonComp">
+                <div className={classname} >
+                    <button type={type} id={id}>{text}</button>
+                </div>
+        </div>
+  );
+};
